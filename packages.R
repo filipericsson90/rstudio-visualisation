687e3d8757f448f2d0939f66c418439d732041e6
Filip E <50950849+filipericsson90@users.noreply.github.com>
2020-10-28 10:40:28 +0100

    Update and rename packages to packages.R

--- packages.R
+++ packages.R
@@ -1,6 +1,6 @@
 # Useful packages to read into R. Will check if they are installed, and if not, they are installed.
 
-pakker <- c("tidyverse", "Hmisc", "survival", "cowplot", "forcats", "GGally", "ggExtra", "foreign", "Rmisc", "validate", "Publish", "colortools")
+pakker <- c("tidyverse", "Hmisc", "survival", "cowplot", "forcats", "GGally", "ggExtra", "foreign", "Rmisc", "validate", "Publish", "colortools", "epiR")
 lapply(pakker, require, character.only = T)
 
 new.packages <- pakker[!(pakker %in% installed.packages()[,"Package"])]
