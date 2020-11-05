cdd0b075403aa76bdecea3918140b00ae9f1efe5
Filip E <50950849+filipericsson90@users.noreply.github.com>
2020-11-05 10:34:37 +0100

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,7 +1,13 @@
-# Useful packages to read into R. Will check if they are installed, and if not, they are installed.
+# Source code and setup
 
-pakker <- c("tidyverse", "Hmisc", "survival", "cowplot", "forcats", "GGally", "ggExtra", "foreign", "Rmisc", "validate", "Publish", "colortools", "epiR")
-lapply(pakker, require, character.only = T)
+pakker <- c("tidyverse", "Hmisc", "survival", "cowplot", 
+            "forcats", "GGally", "ggExtra", "foreign", 
+            "Rmisc", "validate", "Publish", "colortools", "esquisse", "survminer")
 
 new.packages <- pakker[!(pakker %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+
+# Indlæs pakker:
+lapply(pakker, require, character.only = T)
+
+rm(new.packages, pakker)
